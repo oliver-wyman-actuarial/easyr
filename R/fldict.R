bd362216384a9ac6f76c7f04e4b609521682694f
OWG\bryce.chamberlain <bryce.chamberlain@oliverwyman.com>
2022-07-19 13:08:28 -0500

    Update fldict

--- R/fldict.R
+++ R/fldict.R
@@ -25,105 +25,135 @@
 #' fl$sheets
 #' fl$columns
 #'
-fldict = function( folder = NULL, file.list = NULL, pattern = '^[^~]+[.](xls[xmb]?|csv|rds|xml)', ignore.case = TRUE, recursive = TRUE, ...  ){
-
-    if( is.null(file.list) ) if( is.null(folder) ){
-        stop( 'Both folder and file.list have been omitted. Please pass one or the other.' )
+fldict = function( 
+  folder = NULL, 
+  file.list = NULL, 
+  pattern = '^[^~]+[.](xls[xmb]?|csv|rds|xml)', 
+  ignore.case = TRUE, 
+  recursive = TRUE, 
+  verbose = FALSE,
+  ...  
+){
+  
+  if( is.null(file.list) ) if( is.null(folder) ){
+    stop( 'Both folder and file.list have been omitted. Please pass one or the other.' )
+  } else {
+    file.list = list.files( folder, pattern = pattern, ignore.case = ignore.case, recursive = recursive )
+  }
+  
+  if( length( file.list ) == 0 && !is.null( folder ) ){
+    warning( glue::glue( 'fldict: No files found at path: [ {folder} ].' ) )
+    return(NULL)
+  }
+  
+  dl = NULL
+  
+  i = 0
+  for( ifile in file.list ){
+    
+    i = i + 1
+    if(verbose) cat(glue::glue('Reading file {i} of {length(file.list)}: {ifile}\n\n'))
+    
+    # handle excel
+    if( grepl( '[.](xls[xmb]?|xml)', ifile, ignore.case = TRUE ) ){
+      
+      # get sheets.
+      
+      filename = filename.helper( ifile, folder )
+      
+      # attempt standard excel read.
+      sheets = tryCatch( 
+        readxl::excel_sheets( filename ),
+        error = function(e) NULL
+      )
+      
+      # attempt HTML/XML saved as XLS read.
+      if( is.null(sheets) && grepl( '[.]xls$', ifile, ignore.case = TRUE ) ) sheets = 1:length( XML::readHTMLTable( filename ) )
+      
+      if( is.null(sheets) ){
+        
+        dl %<>% dplyr::bind_rows(data.frame(
+          file = ifile,
+          err = 'Could not find sheets for Excel file. This could be due to password protection. Consider un-protecting the file.',
+          stringsAsFactors = FALSE
+        ))
+        
+        warning(glue::glue('
+            Could not find sheets for Excel file at [ {ifile} ]. 
+            This could be due to password protection. Consider un-protecting the file.
+            Warning easyr::fldict() W1228.
+        '))
+        
+        next
+        
+      }
+      
+      if(verbose) cat(glue::glue('\t found {length(sheets)} sheets\n\n'))
+      
+      for( isheet in sheets ){
+        
+        idt = tryCatch({
+          
+          x = read.any( ifile, folder = folder, sheet = isheet, ... )
+          
+          d = dict(x)
+          d$rows = nrow(x)
+          d$cols = ncol(x)
+          
+          d
+          
+        }, error = function(e) data.frame( err = as.character(e) )
+        )
+        
+        idt$file = ifile
+        idt$sheet = isheet
+        
+        dl = bindf( dl, idt )
+        
+        rm( isheet, idt )
+        if( exists('x') ) rm(x)
+        
+      }
+      
     } else {
-        file.list = list.files( folder, pattern = pattern, ignore.case = ignore.case, recursive = recursive )
-    }
-
-    if( length( file.list ) == 0 && !is.null( folder ) ){
-        warning( glue::glue( 'fldict: No files found at path: [ {folder} ].' ) )
-        return(NULL)
-    }
-
-    dl = NULL
-
-    for( ifile in file.list ){
+      
+      idt = tryCatch({
         
-        # handle excel
-        if( grepl( '[.](xls[xmb]?|xml)', ifile, ignore.case = TRUE ) ){
-            
-            # get sheets.
-
-                filename = filename.helper( ifile, folder )
-            
-                # attempt standard excel read.
-                sheets = tryCatch( 
-                    readxl::excel_sheets( filename ),
-                    error = function(e) NULL
-                )
-                    
-                # attempt HTML/XML saved as XLS read.
-                if( is.null(sheets) && grepl( '[.]xls$', ifile, ignore.case = TRUE ) ) sheets = 1:length( XML::readHTMLTable( filename ) )
-                
-                if( is.null(sheets) ) stop( glue::glue( 'Could not find sheets for Excel file at [ {ifile} ].' ) )
-                    
-            for( isheet in sheets ){
-                
-            idt = tryCatch({
-                
-                x = read.any( ifile, folder = folder, sheet = isheet, ... )
-
-                d = dict(x)
-                d$rows = nrow(x)
-                d$cols = ncol(x)
-
-                d
-                
-            }, error = function(e) data.frame( err = as.character(e) )
-            )
-            
-            idt$file = ifile
-            idt$sheet = isheet
-            
-            dl = bindf( dl, idt )
-            
-            rm( isheet, idt )
-            if( exists('x') ) rm(x)
-            
-            }
-            
-        } else {
-            
-            idt = tryCatch({
-                
-                x = read.any( ifile, folder = folder, ... )
-                
-                d = dict(x)
-                d$rows = nrow(x)
-                d$cols = ncol(x)
-                
-                d
-            
-            }, error = function(e) data.frame( err = as.character(e) )
-            )
-            
-            idt$file = ifile
-            
-            dl = bindf( dl, idt )
-            
-            rm( idt )
-            if( exists('x') ) rm(x)
-            
-        }
+        x = read.any( ifile, folder = folder, ... )
         
-        rm(ifile)
-    
+        d = dict(x)
+        d$rows = nrow(x)
+        d$cols = ncol(x)
+        
+        d
+        
+      }, error = function(e) data.frame( err = as.character(e) )
+      )
+      
+      idt$file = ifile
+      
+      dl = bindf( dl, idt )
+      
+      rm( idt )
+      if( exists('x') ) rm(x)
+      
     }
     
-    for( icol in intersect( c( 'rows', 'cols' ), colnames(dl) ) ) dl[[icol]] = fmat( dl[[icol]] )
-
-    dl = dl[ , unique( c(
-        intersect( c( 'file', 'sheet', 'err', 'rows', 'cols' ), colnames(dl) ),
-        colnames(dl)
-    )) ]
+    rm(ifile)
     
-    return(list(
-        sheets = dplyr::distinct( dl[ , intersect( c( 'file', 'sheet', 'err', 'rows', 'cols' ), colnames(dl) ) ] ),
-        columns = dl
-    ))
-    
+  }
+  
+  for( icol in intersect( c( 'rows', 'cols' ), colnames(dl) ) ) dl[[icol]] = fmat( dl[[icol]] )
+  
+  dl = dl[ , unique( c(
+    intersect( c( 'file', 'sheet', 'err', 'rows', 'cols' ), colnames(dl) ),
+    colnames(dl)
+  )) ]
+  
+  return(list(
+    sheets = dplyr::distinct( dl[ , intersect( c( 'file', 'sheet', 'err', 'rows', 'cols' ), colnames(dl) ) ] ),
+    columns = dl
+  ))
+  
 }
 
