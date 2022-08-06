2a38bd104c8025556f6c053414bf441f3aeaa3cd
Ken <ken.zesso@oliverwyman.com>
2022-08-05 19:01:36 -0500

    add optional "keep" parameter to begin

--- R/begin.R
+++ R/begin.R
@@ -4,6 +4,7 @@
 #'
 #' @param wd Path to set as working directory. If blank, the location of the current file open in RStudio will be used if available. If FALSE, the working directory will not be changed.
 #' @param load Packages to load. If not available, they'll be installed.
+#' @param keep Environment objects to keep. If blank, all user-defined objects will be removed from the environment.
 #' @param scipen Do scientific notation in output?
 #' @param verbose Print information about what the function is doing?
 #' @param repos choose the URL to install from.
@@ -16,14 +17,31 @@
 begin = function(
   wd = NULL, 
   load = c( 'magrittr', 'dplyr' ),
+  keep = NULL,
   scipen = FALSE,
   verbose = TRUE,
   repos = 'http://cran.us.r-project.org'
 ){
   
   # clear workspace.
-  rm( list = ls( all.names = TRUE, envir = parent.frame() ), envir = parent.frame() )
-  if( verbose ) cat( 'Cleared variables from environment. \n' )
+  envobjs = ls( all.names = TRUE, envir = parent.frame() )
+  drop = setdiff( envobjs, keep )
+  # check if attempting to keep any objects not in environment
+  if( length( keep ) > 0 ){
+    missing = setdiff( keep, envobjs )
+    if( length( missing ) > 0 ){ 
+      stop( 
+        paste0( 'Attempting to keep objects not in environment: [', 
+        paste0( missing, collapse = ', ' ), ']. ' 
+      )) 
+    }
+  }
+  # clear vars not set to keep
+  rm( list = drop, envir = parent.frame() )
+  if( verbose ) cat( 
+    'Cleared variables from environment: [', 
+    paste0( drop, collapse = ', ' ), ']. \n' 
+  )
   
   # set working directcory.
   
