6c70f20190547e2e916335d2067eebec09926b7e
duncan.kryszyn@oliverwyman.com <duncan.kryszyn@oliverwyman.com>
2023-04-03 15:17:46 -0500

    Issue #43

--- tests/testthat/test_ecopy.R
+++ tests/testthat/test_ecopy.R
@@ -1,4 +1,4 @@
-test_that("works as expected", {
+test_that("without buffer", {
 
   # data frame.
   ecopy( iris, showrowcolnames = "cols", show = 'show' )
@@ -22,4 +22,30 @@ test_that("works as expected", {
     
   }
 
+})
+
+test_that("with buffer", {
+  
+  # data frame.
+  ecopy( iris, showrowcolnames = "cols", show = 'show', buffer = 5000 )
+  ecopy( iris )
+  
+  # string.
+  ecopy( 'hello' )
+  
+  # reading from the clipboard is not always possible.
+  result = tryCatch({
+    readClipboard(format = 1, raw = FALSE)
+  },
+  error = function(e) {}
+  )
+  if(!is.null(result) && result != '??'){ # r-devel system seems to return '??' from readClipboard.
+    
+    expect_equal(result, 'hello')    
+    
+    # errors.
+    expect_error( { ecopy( iris, showrowcolnames = "wrong", show = 'show', buffer = 5000 ) }, 'should be one of' )
+    
+  }
+  
 })
